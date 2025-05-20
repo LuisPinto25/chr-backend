a520743d54f5f115325235e6aa024db6ffea1f47
Luis Pinto <dapin46@hotmail.com>
2025-05-20 17:23:40 -0500

    Add: Cors added to the server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { type Request, type Response } from 'express';
+import cors from 'cors';
 import userRouter from './routers/userRouter';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
